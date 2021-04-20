09ee2234d30dd1892224a1d16eb2c2ec4d4792bf
yl3030 <yl301114@gmail.com>
2021-04-20 15:00:04 +0800

    0420-2

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,8 @@
 $("#menu-icon").click(function () {
+    var NavHT = $(".header-nav").height()*(-1);
 	if ($(this).hasClass("active")) {
+		$(".header-nav").css("top", NavHT);
 		$(this).removeClass("active");
-		$(".header-nav").css("top", "-100%");
 	} else {
 		$(this).addClass("active");
 		$(".header-nav").css("top", "60px");
